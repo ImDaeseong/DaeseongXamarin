8c907bf1b69cb6b26f7ef8f544e965f8d5b9e747
ImDaeseong <cs93059@gmail.com>
2017-08-10 14:42:08 +0900

    DaeseongXamarin

---
+++ Rg.Plugins.Popup/App1/App1/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Rg.Plugins.Popup.Extensions;
+using System;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnok_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushPopupAsync(new Popup());
+        }
+    }
+}
---
+++ Rg.Plugins.Popup/App1/App1/Popup.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Rg.Plugins.Popup.Pages;
+using Rg.Plugins.Popup.Services;
+using System.Threading.Tasks;
+
+namespace App1
+{   
+    public partial class Popup : PopupPage
+    {
+        public Popup()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            //HidePopup();
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            
+        }
+
+        private async void HidePopup()
+        {
+            await Task.Delay(4000);
+            await PopupNavigation.RemovePageAsync(this);
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            return true;
+        }
+
+    }
+}

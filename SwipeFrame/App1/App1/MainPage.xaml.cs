4e02708fcc15863861ed67368a6adcf91517d152
ImDaeseong <cs93059@gmail.com>
2017-08-16 16:40:32 +0900

    DaeseongXamarin

---
+++ SwipeFrame/App1/App1/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            GestureFrame gi = new GestureFrame
+            {
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                BackgroundColor = Color.FromHex("bf3122"),
+            };
+
+            gi.SwipeDown += (s, e) =>
+            {
+                DisplayAlert("Gesture Info", "Swipe Down Detected", "OK");
+            };
+
+            gi.SwipeTop += (s, e) =>
+            {
+                DisplayAlert("Gesture Info", "Swipe Top Detected", "OK");
+            };
+
+            gi.SwipeLeft += (s, e) =>
+            {
+                DisplayAlert("Gesture Info", "Swipe Left Detected", "OK");
+            };
+
+            gi.SwipeRight += (s, e) =>
+            {
+                DisplayAlert("Gesture Info", "Swipe Right Detected", "OK");
+            };
+            this.Content = gi;
+
+        }
+    }
+}
---
+++ SwipeFrame/App1/App1/Page1.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();                        
+        }
+
+        private void swipeFrame_SwipeDown(object sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("swipeFrame_SwipeDown");
+        }
+
+        private void swipeFrame_SwipeTop(object sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("swipeFrame_SwipeTop");
+        }
+
+        private void swipeFrame_SwipeLeft(object sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("swipeFrame_SwipeLeft");
+        }
+
+        private void swipeFrame_SwipeRight(object sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("swipeFrame_SwipeRight");
+        }
+    }
+}

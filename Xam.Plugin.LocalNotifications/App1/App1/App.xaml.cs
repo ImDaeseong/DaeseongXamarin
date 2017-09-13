179d9071c3ddcc24439766b3f4e93fbb38af85dc
ImDaeseong <cs93059@gmail.com>
2017-09-13 16:08:16 +0900

    DaeseongXamarin

---
+++ Xam.Plugin.LocalNotifications/App1/App1/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Xamarin.Forms;
+using LocalNotifications.Plugin;
+using LocalNotifications.Plugin.Abstractions;
+
+namespace App1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new App1.MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            var notifier = CrossLocalNotifications.CreateLocalNotifier();
+            notifier.Notify(new LocalNotification()
+            {
+                Title = "Title",
+                Text = "Text",
+                Id = 1,
+                NotifyTime = DateTime.Now.AddSeconds(10),
+            });
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ Xam.Plugin.LocalNotifications/App1/App1/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Xamarin.Forms;
+using LocalNotifications.Plugin;
+using LocalNotifications.Plugin.Abstractions;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Clicked(object sender, EventArgs e)
+        {
+            var notification = new LocalNotification
+            {
+                Title = "Title",
+                Text = "Text",
+                Id = 2,
+                NotifyTime = DateTime.Now,
+            };
+
+            var notifier = CrossLocalNotifications.CreateLocalNotifier();
+            notifier.Notify(notification);
+        }
+
+        private void Button2_Clicked(object sender, EventArgs e)
+        {            
+            var notifier = CrossLocalNotifications.CreateLocalNotifier();
+            notifier.Notify(new LocalNotification()
+            {
+                Title = "",
+                Text = "Text",
+                Id = 3,
+                NotifyTime = DateTime.Now.AddSeconds(1),
+            });
+        }
+
+    }
+}

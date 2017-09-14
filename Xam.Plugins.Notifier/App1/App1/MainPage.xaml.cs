6a024729cfc05f2abd93d8eb9a711578cb16936c
ImDaeseong <cs93059@gmail.com>
2017-09-14 09:02:40 +0900

    DaeseongXamarin

---
+++ Xam.Plugins.Notifier/App1/App1/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Plugin.LocalNotifications.Abstractions;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        private ILocalNotifications localNotifications;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            localNotifications = DependencyService.Get<ILocalNotifications>();
+        }
+
+        private void showNotificationButton_Clicked(object sender, EventArgs e)
+        {
+            localNotifications.Show("Title", "Body", 1);
+        }
+
+        private void cancelNotificationButton_Clicked(object sender, EventArgs e)
+        {
+            localNotifications.Cancel(1);
+        }
+
+    }
+}
